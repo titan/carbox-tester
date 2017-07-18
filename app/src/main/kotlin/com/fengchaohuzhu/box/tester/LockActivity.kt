e9fdbd505cc27561817eff6994ebd2a973403159
Titan <howay.tan@gmail.com>
2017-07-18 17:15:23 +0800

    Do all operations in worker thread

--- app/src/main/kotlin/com/fengchaohuzhu/box/tester/LockActivity.kt
+++ app/src/main/kotlin/com/fengchaohuzhu/box/tester/LockActivity.kt
@@ -2,7 +2,8 @@ package com.fengchaohuzhu.box.tester
 
 import java.io.File
 import java.io.FilenameFilter
-import java.util.LinkedList
+import java.util.Comparator
+import java.util.PriorityQueue
 import java.util.Queue
 import java.util.Timer
 
@@ -39,7 +40,6 @@ enum class LockViewID (val value: Int) {
 
 class LockActivity : Activity(), AnkoLogger {
   var board: Byte = 0
-  var sdk: LockerSDK? = null
   var worker: Worker? = null
   var locktimer: Timer? = null
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -78,6 +78,12 @@ class LockActivity : Activity(), AnkoLogger {
 
   fun scan(port: SerialPort) {
     doAsync() {
+      uiThread {
+        find<Spinner>(LockViewID.DEVICES.value).setEnabled(false)
+        find<TextView>(LockViewID.ERROR.value).text = ""
+        find<View>(LockViewID.ERROR_CONTAINER.value).visibility = View.INVISIBLE
+        find<GridView>(LockViewID.BOXES.value).visibility = View.INVISIBLE
+      }
       worker?.running = false
       locktimer?.cancel()
       port.setBaudRate(9600)
@@ -94,48 +100,48 @@ class LockActivity : Activity(), AnkoLogger {
       } else {
         var dialog: ProgressDialog? = null
         uiThread {
-          dialog = progressDialog(title = "扫描锁控板")
+          dialog = progressDialog(title = "扫描锁控板", message = "")
           dialog?.max = 16
           dialog?.show()
         }
-        var found = false
-        sdk = LockerSDK(port.getInputStream(), port.getOutputStream())
-        for (i in 1..16) {
-          dialog?.setMessage("检测${i}号锁控板")
-          dialog?.progress = i
-          if (sdk?.is_online(i.toByte()) ?: false) {
+        worker = Worker(port)
+        worker?.start()
+        worker?.queue?.add(LockCommand(type = LockCommandType.SCAN) { dat: Any? ->
+          val pair: Pair<Byte, Boolean> = dat as Pair<Byte, Boolean>
+          if (pair.second) {
+            board = pair.first
             uiThread {
               dialog?.cancel()
-              toast("发现${i}号锁控板")
+              toast("发现${pair.first}号锁控板")
             }
-            found = true
-            board = i.toByte()
-            break
+            refreshLockGridUI(IntArray(0))
+            locktimer = timer(initialDelay = 0L, period = 1000L) {
+              worker?.queue?.add(LockCommand(type = LockCommandType.QUERY, board = board) { dat1: Any? ->
+                refreshLockGridUI(dat1 as IntArray)
+              })
+            }
+          } else {
+           if (pair.first == 0.toByte()) {
+              port.closePort()
+              uiThread {
+                dialog?.cancel()
+                find<Spinner>(LockViewID.DEVICES.value).setEnabled(true)
+                find<TextView>(LockViewID.ERROR.value).text = "在 ${port.systemPortName} 上未发现可用的锁控板"
+                find<View>(LockViewID.ERROR_CONTAINER.value).visibility = View.VISIBLE
+                find<GridView>(LockViewID.BOXES.value).visibility = View.INVISIBLE
+              }
+            } else {
+              uiThread {
+                dialog?.setMessage("检测${pair.first}号锁控板")
+                dialog?.progress = pair.first.toInt()
+              }
+            }
           }
-        }
-        if (!found) {
-          sdk = null
-          port.closePort()
-          uiThread {
-            dialog?.cancel()
-            find<Spinner>(LockViewID.DEVICES.value).setEnabled(true)
-            find<TextView>(LockViewID.ERROR.value).text = "在 ${port.systemPortName} 上未发现可用的锁控板"
-            find<View>(LockViewID.ERROR_CONTAINER.value).visibility = View.VISIBLE
-            find<GridView>(LockViewID.BOXES.value).visibility = View.INVISIBLE
-          }
-        } else {
-          worker = Worker(sdk!!)
-          worker?.start()
-
-          locktimer = timer(period = 3000L) {
-            worker?.queue?.add(LockCommand(type = LockCommandType.QUERY, board = board) { dat: Any? ->
-              refreshLockGridUI(dat as IntArray)
-            })
-          }
-        }
+        })
       }
     }
   }
+
   fun open(lock: Byte) {
     worker?.queue?.add(LockCommand(type = LockCommandType.OPEN, board = board, lock = lock) { _: Any? ->
       worker?.queue?.add(LockCommand(type = LockCommandType.QUERY, board = board) { dat: Any? ->
@@ -175,7 +181,6 @@ class LockActivityUi : AnkoComponent<LockActivity> {
       spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
         override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
           ui.owner.scan(spinner.adapter.getItem(pos) as SerialPort)
-          spinner.setEnabled(false)
         }
 
         override fun onNothingSelected(parent: AdapterView<out Adapter>?) {
@@ -293,20 +298,32 @@ class BoxAdapter(val boxes: List<Box>) : BaseAdapter() {
   }
 }
 
-enum class LockCommandType {
-  QUERY,
-  CHECK,
-  OPEN
+enum class LockCommandType(val value: Int) {
+  CHECK(3),
+  OPEN(1),
+  QUERY(3),
+  SCAN(7)
 }
 
 data class LockCommand (val type: LockCommandType = LockCommandType.QUERY, val board: Byte = 1, val lock: Byte = 1, val callback: (result: Any?) -> Unit)
 
-class Worker(val sdk: LockerSDK): Thread() {
-  val queue: Queue<LockCommand> = LinkedList<LockCommand>()
+class Worker(val port: SerialPort): Thread() {
+  val queue: Queue<LockCommand> = PriorityQueue<LockCommand>(11, object: Comparator<LockCommand> {
+    override fun compare(a: LockCommand, b: LockCommand): Int {
+      if (a.type.value < b.type.value) {
+        return -1
+      } else if (a.type.value > b.type.value) {
+        return 1
+      } else {
+        return 0
+      }
+    }
+  })
   var running: Boolean = true
 
   override fun run() {
-    while (running) {
+    val sdk: LockerSDK = LockerSDK(port.getInputStream(), port.getOutputStream())
+    while (running && port.isOpen()) {
       try {
         Thread.sleep(1000)
       } finally {
@@ -316,7 +333,24 @@ class Worker(val sdk: LockerSDK): Thread() {
         LockCommandType.QUERY -> cmd.callback(sdk.query(cmd.board))
         LockCommandType.CHECK -> cmd.callback(sdk.check(cmd.board))
         LockCommandType.OPEN -> cmd.callback(sdk.open(cmd.board, cmd.lock))
+        LockCommandType.SCAN -> {
+          var found = false
+          for (i in 1..16) {
+            cmd.callback(Pair<Byte, Boolean>(i.toByte(), false)) // just publish progress
+            if (sdk.is_online(i.toByte())) {
+              cmd.callback(Pair<Byte, Boolean>(i.toByte(), true))
+              found = true
+              break
+            }
+          }
+          if (!found) {
+            cmd.callback(Pair<Byte, Boolean>(0.toByte(), false))
+          }
+        }
       }
+    }
+    if (port.isOpen()) {
+      port.closePort()
     }
   }
 }
